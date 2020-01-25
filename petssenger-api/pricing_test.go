224cfae9ce03632b08e10b5d1ea7906243687d79
Walter Lopes <walter_vinicius@outlook.com>
2020-01-25 11:56:07 -0300

    Created first test in go

---
+++ petssenger-api/pricing_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	. "./pricing"
+)
+
+func TestCalculatorPricing(t *testing.T) {
+	p := Pricing{
+		City:           "São Paulo",
+		BaseFee:        3.50,
+		PricePerMinute: 1.00,
+		ServiceFee:     0.75,
+		PricePerKm:     0.50,
+	}
+	total := p.Calc(3, 10, 1)
+
+	if total != 15.75 {
+		t.Errorf("Sum was incorrect, got: %b, want: %b.", total, 15.75)
+	}
+
+}
